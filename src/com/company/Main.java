b1316c504ff9f0a8f4870964b7d9a0fd502af80c
alexkappa1 <aleksandra.kapadze@gmail.com>
2022-02-25 21:10:27 +0200

    hi

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number;
+        number = 4;
+        int value = 6;
+	System.out.println("Greetings, all");
+    System.out.println("Addition of number and value is: " + (number + value));
+    System.out.println("Multiplication of number and value is: " + (number * value));
+
+    }
+}
